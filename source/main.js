b766d708e43d455893cb3095aeb102397943cbc4
Anadian <supercamman20@gmail.com>
2017-10-28 11:39:04 -0400

    New things?

--- source/main.js
+++ source/main.js
@@ -1,4 +1,4 @@
-#!/usr/local/bin/node || /usr/bin/env node
+#!/usr/local/bin/node
 /**
 *	@file jsmacro/source/smain.js
 *	@brief A simple, progressive macro expansions engine written in nodejs.
@@ -26,7 +26,7 @@ const Log = require('./log.js');
 //Standard
 const FileSystem = require('fs');
 const Path = require('path');
-const Utility = require('utility');
+const Utility = require('util');
 //External
 const CommandLineArgs = require('command-line-args');
 const CommandLineUsage = require('command-line-usage');
@@ -36,8 +36,7 @@ const OptionDefinitions = [
 	{name: 'version', alias: 'V', type: Boolean, description: 'Display version information and exit.'},
 	{name: 'input', alias: 'I', type: String, description: 'File to process.'},
 	{name: 'rules', alias: 'R', type: String, description: 'File to read text-transformation rules from.'},
-	{name: 'output', alias: 'O', type: String, description: 'File to write processed output to.'},
-	{}
+	{name: 'output', alias: 'O', type: String, description: 'File to write processed output to.'}
 ];
 
 const Options = CommandLineArgs(OptionDefinitions);
@@ -47,27 +46,28 @@ function ParseRules(rules_file){
 	var rules_map = new Map();
 	var rules_string = FileSystem.readFileSync(rules_file,'utf8');
 	var rules_strings = rules_string.split('\n');
+	Log.log(process.argv0,'jsmacro',Path.basename(__filename),'ParseRules','debug',Utility.format(rules_strings));
 	if(rules_strings != null){
 		for(var i = 0; i < rules_strings.length; i++){
-			var matches = rules_strngs[i].match(/^\$(\w*)=\/(.*)\/\[(\d)\]$/);
+			var matches = rules_strings[i].match(/^\$(\w*)=\/(.*)\/\[(\d)\]$/);
 			if(matches != null){
 				var number = parseInt(matches[3],10);
 				if(number != NaN){
 					rules_map.set(matches[1],{name: matches[1], type: 'assignment', regex: new RegExp(matches[2],'g'), subpattern: number, value: null});
 				} else{
-					console.error('Error: Unrecognized number for rule #%d, \'%s\' ', i, matches[3], matches);
+					Log.log(process.argv0,'jsmacro','main.js','ParseRules','error',Utility.format('Unrecognized number for rule #%d, \'%s\' ', i, matches[3], matches));
 				}
 			} else{
 				matches = rules_strings[i].match(/^\/(.*)\/=\$(\w*)$/);
 				if(matches != null){
 					rules_map.set(matches[0],{name: matches[0], type: 'application', regex: new RegExp(matches[1],'g'), key: matches[2]});
 				} else{
-					console.error('Error: Couldn\'t interperet rule on line %d: %s', i, rules_strings[i]); 
+					Log.log(process.argv0,'jsmacro','main.js','ParseRules','error',Utility.format('Couldn\'t interperet rule on line %d: %s', i, rules_strings[i])); 
 				}
 			}
 		}
 	} else{
-		console.error('Error: Problem reading rules file \'%s\'.', rules_file);
+		Log.log(process.argv0,'jsmacro','main.js','ParseRules','error',Utility.format('Problem reading rules file \'%s\'.', rules_file));
 	}
 	return rules_map;
 }
@@ -92,26 +92,27 @@ function ParseInput(input_file,rules_map){
 							map.set(key, object);
 						}
 					} else if(value.type === 'application'){
-						match = input_strings[i].match(value.regex)
+						match = input_strings[i].match(value.regex);
 						if(match != null){
 							var applied_value = map.get(value.key);
 							input_strings[i].replace(value.regex,applied_value.value);
 						}
 					} else {
-						console.error('Error: Unkown type for rule map key \'%s\', \'%s\'.', key, value.type, value);
+						Log.log(process.argv0,'jsmacro','main.js','ParseInput','error',Utility.format('Unkown type for rule map key \'%s\', \'%s\'.', key, value.type, value));
 					}
 					j++;
 				} else{
 					forEach_finished = true;
 				}
 			} //forEach_Callback
+			);
 			while(forEach_finished != true);
 		} //for loop
 		var output_string = input_strings.join('\n');
 		_return = [1,output_string];
 	} else{
-		var error_message = Utility.format('Error: A problem occurred while trying to read file \'%s\'', input_file);
-		console.error(error_message);
+		var error_message = Utility.format('A problem occurred while trying to read file \'%s\'', input_file);
+		Log.log(process.argv0,'jsmacro','main.js','ParseInput','error',Utility.format(error_message));
 		_return = [0,error_message];
 	}
 	return _return;
@@ -128,4 +129,19 @@ function main(){
 					var output = ParseInput_return[1];
 					FileSystem.writeFileSync(Options.output, output);
 				} else{
-					
+					Log.log(process.argv0,'jsmacro','main.js','main','error',Utility.format('Parsing input was unsuccessful.', ParseInput_return));
+				}
+			} else{
+				Log.log(process.argv0,'jsmacro','main.js','main','error',Utility.format('Invalid or absent value for input file or output file. Input file: \'%s\' output file: \'%s\'', Options.input, Options.output));
+			}
+		} else{
+			Log.log(process.argv0,'jsmacro','main.js','main','error',Utility.format('Parsing rules was unsuccessful.', ParseRules_return));
+		}
+	} else{
+		Log.log(process.argv0,'jsmacro','main.js','main','error',Utility.format('Invalid or absent value for rukes file: \'%s\'', Options.rules));
+	}
+}
+
+if(require.main === module){
+	main();
+}
